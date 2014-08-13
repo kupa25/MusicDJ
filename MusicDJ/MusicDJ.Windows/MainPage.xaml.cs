77bcb7f59a5516fb1d494576912c6fb2ede86cb2
kupa25@gmail.com <haran802>
2014-08-13 08:33:47 -0400

    initial commit

---
+++ MusicDJ/MusicDJ.Windows/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MusicDJ
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            xboxApiCall();
+        }
+
+        private void xboxApiCall()
+        {
+            
+        }
+    }
+}
